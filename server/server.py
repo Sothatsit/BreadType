19779b2b5ddb7b5c3205aafbbcb30a0df2fda3c5
Padraig (Paddy) Lamont <22494652@student.uwa.edu.au>
2020-04-23 23:25:16 +0800

    Serve a basic web page

--- server/server.py
+++ server/server.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+    return render_template("index.html")
+
+@app.route('/<path:path>')
+def static_response(path):
+    return send_from_directory(app.static_folder, path)
