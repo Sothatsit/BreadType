4bf34de96f4b7d37dce8546e3af938931995b8f3
sam9783 <sam9783@users.noreply.github.com>
2020-05-25 23:24:26 +0800

    Added functionality to delete users

--- server/main_routes.py
+++ server/main_routes.py
@@ -5,8 +5,9 @@ Manages the routes for the main entry-points to the website.
 from flask_login import login_required, current_user
 from flask import Blueprint, render_template, send_from_directory, current_app, flash, request
 from .user_model import requires_role, load_user, load_all_users
-from .quiz_model import load_quiz
-from .error_routes import not_found
+from .quiz_model import load_quiz, delete_db_quiz
+from .error_routes import not_found, message_page
+from . import db
 
 
 main = Blueprint("main", __name__)
@@ -43,6 +44,36 @@ def admin():
     return render_template("admin.html", 
         title="Admin", 
         users=users)
+
+
+@main.route("/profile/<int:user_id>/delete")
+@requires_role("admin")
+def delete_user(user_id):
+    """
+    If a user navigates to this page and has permission, deletes the user.
+    """
+    # Find the user.
+    user = load_user(user_id)
+    if user is None:
+        flash("The user you were looking for could not be found.")
+        return not_found()
+
+    # Find and delete all user's quizes
+    quizzes = user.get_quizzes()
+    for quiz in quizzes:
+        delete_db_quiz(quiz)
+
+    # Find and delete all user's answers
+    for db_answer in user.db_answers:
+        db.session.delete(db_answer)
+
+    # Delete the user.
+    db.session.delete(user)
+    db.session.commit()
+    
+    # Tell the admin the user has been deleted.
+    message = "The user \"{}\" has been deleted.".format(user.name)
+    return message_page("User Deleted", message)
 
 
 @main.route("/<path:path>")
